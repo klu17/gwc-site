9258948bfc75df353fe4e994758c422762f77973
Karmen <lu.kar@husky.neu.edu>
2020-07-14 22:19:37 -0400

    Add links to Resources Page, description to Home page, and details to About Us page.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
- 
+
 class Home extends Component {
   render() {
     return (
       <div>
         <h2>Home</h2>
-        <p>Home. Sweet. Home.</p>
+        <section>
+          <p>We are Girls Who Code of Grove Hall. Girls Who Code is a non-profit organization working to
+            close the gender gap in technology. Typically, we hold in-person workshops every other Saturday
+            throughout an academic semester to teach middle schoolers and high schoolers the basics of computer
+            programming. Together, we have done step-by-step lectures on inclusive design, generated random shapes with Python,
+            mixed music with EarSketch, covered the basics of Java, and much more. We're a lot of fun. :)
+        </p>
+          <p>
+            The purpose of this site is to stay connected with you. You'll find resources for learning at home and hear about our upcoming events.
+        </p>
+        </section>
       </div>
     );
   }
 }
- 
+
 export default Home;
