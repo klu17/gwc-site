36df1a31456efbe6c4ec592524b9f17bc2ccbbb3
Karmen <lu.kar@husky.neu.edu>
2020-07-14 22:49:31 -0400

    Edit inline style for calendar iframe.

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -6,7 +6,7 @@ class Calendar extends Component {
       <div>
         <h2>Calendar</h2>
         <p>Here is a calendar of our upcoming events.</p>
-        <iframe src="https://calendar.google.com/calendar/embed?src=ophbq5agj0k356fte9u3npvou4%40group.calendar.google.com&ctz=America%2FNew_York" style={{border: 0}} width="800" height="600" frameBorder="0" scrolling="no"></iframe>
+        <iframe src="https://calendar.google.com/calendar/embed?src=ophbq5agj0k356fte9u3npvou4%40group.calendar.google.com&ctz=America%2FNew_York" style={{border: 0, width:800, height:600, frameBorder:0, scrolling:"no"}}></iframe>
       </div>
     );
   }
