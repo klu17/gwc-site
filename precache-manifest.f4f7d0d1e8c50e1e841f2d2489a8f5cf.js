d7c6e2ce27ceef6a7af38d8d0e79518e0dfbd651
Karmen <lu.kar@husky.neu.edu>
2020-07-14 22:50:20 -0400

    Updates

---
+++ precache-manifest.f4f7d0d1e8c50e1e841f2d2489a8f5cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7eadce952e7bf49aa553696fb3cba6a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f632a50eb88ef385adf9",
+    "url": "/static/css/main.3ada03b2.chunk.css"
+  },
+  {
+    "revision": "7c5b6121d07e03ff15de",
+    "url": "/static/js/2.113e344b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.113e344b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f632a50eb88ef385adf9",
+    "url": "/static/js/main.2273e4da.chunk.js"
+  },
+  {
+    "revision": "aa7f63b921e65e972566",
+    "url": "/static/js/runtime-main.cdb6021f.js"
+  }
+]);
